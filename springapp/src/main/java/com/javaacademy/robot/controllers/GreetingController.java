cb71fb2ad70d9b2dc90fe8eea4a400adaffbc711
Michal <michal_partacz@epam.com>
2017-08-21 18:08:09 +0200

    Removed a password from properties file; Shortened too long line

--- springapp/src/main/java/com/javaacademy/robot/controllers/GreetingController.java
+++ springapp/src/main/java/com/javaacademy/robot/controllers/GreetingController.java
@@ -14,7 +14,8 @@ import java.util.Arrays;
 @Controller
 public class GreetingController {
     @RequestMapping("/greeting")
-    public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+    public String hello(@RequestParam(value = "name", required = false, defaultValue = "World")
+                                    String name, Model model) {
         model.addAttribute("name", name);
         model.addAttribute("exampleBooks", Arrays.asList("Advanced Java", "Clean Code", "Effective Java"));
         return "../static/templates/greeting";
