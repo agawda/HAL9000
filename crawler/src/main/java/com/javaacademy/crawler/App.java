5ee73b822f479c94d3b616439d774bcb9d1b86be
Michal <michal_partacz@epam.com>
2017-08-21 17:37:03 +0200

    Added modules to the app;
    The spring code now is located in springapp module with a separate pom file,
    and the other module is crawler module which will contain all the crawler
    code;

---
+++ crawler/src/main/java/com/javaacademy/crawler/App.java
@@ -0,0 +1,5 @@
+package com.javaacademy.crawler;
+
+public class App {
+    public static void main(String[] args) {}
+}
---
+++ crawler/src/test/java/com/javaacademy/crawler/AppTest.java
@@ -0,0 +1,12 @@
+package com.javaacademy.crawler;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+    @Test
+    public void configTest() {
+        System.out.println("Crawler tests run!");
+    }
+
+}
