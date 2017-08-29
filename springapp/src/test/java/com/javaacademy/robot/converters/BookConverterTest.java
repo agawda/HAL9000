58c6080fbe5b731683b867ae409a8e94916b1951
Michal <michal_partacz@epam.com>
2017-08-29 12:05:37 +0200

    Added tests for collective conversion methods;

--- springapp/src/test/java/com/javaacademy/robot/converters/BookConverterTest.java
+++ springapp/src/test/java/com/javaacademy/robot/converters/BookConverterTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -46,5 +47,32 @@ public class BookConverterTest {
 
         Book converted = bookConverter.toEntity(bookDto);
         assertTrue(converted.equals(book));
+    }
+
+    @Test
+    public void testCollectiveConversions() {
+        Book book1 = new Book();
+        book1.setTitle("Title");
+        book1.setSubtitle("Subtitle1");
+        Book book2 = new Book();
+        book2.setTitle("Title2");
+        book2.setSubtitle("Subtitle2");
+        List<Book> books = new ArrayList<>(Arrays.asList(book1, book2));
+
+        BookConverter bookConverter = new BookConverter();
+
+        List<BookDto> dtos = bookConverter.toDtos(books);
+
+        BookDto bookDto1 = dtos.get(0);
+        BookDto bookDto2 = dtos.get(1);
+
+        assertEquals(bookDto1.getTitle(), book1.getTitle());
+        assertEquals(bookDto1.getSubtitle(), book1.getSubtitle());
+        assertEquals(bookDto2.getTitle(), book2.getTitle());
+        assertEquals(bookDto2.getSubtitle(), book2.getSubtitle());
+
+        List<Book> convertedBooks = bookConverter.toEntities(dtos);
+
+        assertEquals(convertedBooks, books);
     }
 }
