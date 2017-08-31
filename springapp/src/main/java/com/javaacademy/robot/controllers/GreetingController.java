001b2494d47f56bbed4441d2327c5a96c1138669
Michal <michal_partacz@epam.com>
2017-08-31 15:36:14 +0200

    The Books subpage displays book's required info;
    Added a list of books and links to subsites containing book's detalis;
    Added a method which will change the zoom of the image to custom;
    Added bookService to view controller;

--- springapp/src/main/java/com/javaacademy/robot/controllers/GreetingController.java
+++ springapp/src/main/java/com/javaacademy/robot/controllers/GreetingController.java
@@ -1,11 +1,15 @@
 package com.javaacademy.robot.controllers;
 
+import com.javaacademy.robot.model.BookDto;
+import com.javaacademy.robot.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Anna Gawda
@@ -13,6 +17,14 @@ import java.util.Arrays;
  */
 @Controller
 public class GreetingController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public GreetingController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @RequestMapping("/greeting")
     public String hello(@RequestParam(value = "name", required = false, defaultValue = "World")
                                 String name, Model model) {
@@ -24,14 +36,29 @@ public class GreetingController {
     @RequestMapping("/bookstores")
     public String bookstore(@RequestParam(value = "id") String bookstore, Model model) {
         model.addAttribute("id", bookstore);
-        model.addAttribute("bookTitle", "Advanced Java");
+        List<BookDto> books = bookService.getAllBookDtos();
+        model.addAttribute("books", books);
         return "../static/templates/bookstore";
     }
 
     @RequestMapping("/books/")
-    public String book(@RequestParam(value = "id") String bookTitle, Model model) {
-        model.addAttribute("id", bookTitle);
-        model.addAttribute("price", 23.99); //hardcoded example price
+    public String book(@RequestParam(value = "id") Long isbn, Model model) {
+        model.addAttribute("id", isbn);
+        BookDto bookDto = bookService.getBookByIsbn(isbn);
+        setImageZoom(bookDto, 3);
+        model.addAttribute("book", bookDto);
         return "../static/templates/bookview";
+    }
+
+    private void setImageZoom(BookDto bookDto, int zoom) {
+        String s = bookDto.getSmallThumbnail();
+        System.out.println("s = " + s);
+        String zoomString = "&zoom=";
+        if(s.contains(zoomString)) {
+            String zoomValue = s.split(zoomString)[1].split("&")[0];
+            String linkWithNewZoom = s.replace(zoomString + zoomValue, zoomString + zoom);
+            System.out.println("new link = " +linkWithNewZoom);
+            bookDto.setSmallThumbnail(linkWithNewZoom);
+        }
     }
 }
