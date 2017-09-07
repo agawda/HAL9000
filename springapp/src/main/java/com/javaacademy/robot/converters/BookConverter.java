14c5172619475a3e3b2c94263a668f759c77547e
Michal <michal_partacz@epam.com>
2017-09-07 15:54:01 +0200

    Added fields for datetime and bookstore name to db model;

--- springapp/src/main/java/com/javaacademy/robot/converters/BookConverter.java
+++ springapp/src/main/java/com/javaacademy/robot/converters/BookConverter.java
@@ -4,7 +4,10 @@ import com.javaacademy.robot.model.Book;
 import com.javaacademy.robot.model.BookDto;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
@@ -43,6 +46,8 @@ public class BookConverter implements DtoEntityConverter<Book, BookDto> {
         book.setListPriceCurrencyCode(dto.getListPriceCurrencyCode());
         book.setRetailPriceAmount(dto.getRetailPriceAmount());
         book.setRetailPriceCurrencyCode(dto.getRetailPriceCurrencyCode());
+        book.setDateAdded(LocalDateTime.now());
+        book.setShoName(recognizeShopName(dto));
         return book;
     }
 
@@ -55,4 +60,39 @@ public class BookConverter implements DtoEntityConverter<Book, BookDto> {
     public List<BookDto> toDtos(List<Book> entities) {
         return entities.stream().map(this::toDto).collect(Collectors.toList());
     }
+
+    private String recognizeShopName(BookDto bookDto) {
+        String link = bookDto.getCanonicalVolumeLink();
+        if(link.contains("//")) {
+            String shopLinkFirstPart = bookDto.getCanonicalVolumeLink().split("//")[1];
+            String shopAddress = shopLinkFirstPart.split("/")[0];
+            return Shop.getStoreName(shopAddress).toString();
+        }
+        return "";
+    }
+
+    enum Shop {
+        GOOGLE_STORE("market.android.com"),
+        GANDALF("www.gandalf.com.pl"),
+        BONITO("bonito.pl"),
+        CZYTAM("www.czytam.pl"),
+        MATRAS("www.matras.pl"),
+        UNKNOWN("");
+        private static final Map<String, Shop> map = new HashMap<>(values().length, 1);
+
+        static {
+            for (Shop c : values()) map.put(c.storeAddress, c);
+        }
+
+        private String storeAddress;
+
+        Shop(String storeAddress) {
+            this.storeAddress = storeAddress;
+        }
+
+        static Shop getStoreName(String string) {
+            return map.getOrDefault(string, UNKNOWN);
+        }
+    }
+
 }
--- springapp/src/main/java/com/javaacademy/robot/model/Book.java
+++ springapp/src/main/java/com/javaacademy/robot/model/Book.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -60,6 +61,12 @@ public class Book {
 
     @Column(name = "retailPriceCurrencyCode")
     String retailPriceCurrencyCode;
+
+    @Column(name = "shopName")
+    String shoName;
+
+    @Column(name = "dateAdded")
+    LocalDateTime dateAdded;
 
     public Book() {
         //required for ORM
