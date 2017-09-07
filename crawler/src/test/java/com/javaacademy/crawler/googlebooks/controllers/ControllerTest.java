4ac0150ab25c22ea1a4d93a7a0411a420c60368e
Michal <michal_partacz@epam.com>
2017-09-07 10:14:49 +0200

    Refactored code duplication in added test;

--- crawler/src/test/java/com/javaacademy/crawler/googlebooks/controllers/ControllerTest.java
+++ crawler/src/test/java/com/javaacademy/crawler/googlebooks/controllers/ControllerTest.java
@@ -1,8 +1,6 @@
 package com.javaacademy.crawler.googlebooks.controllers;
 
 import com.javaacademy.crawler.googlebooks.dao.GoogleBookEndpoint;
-import com.javaacademy.crawler.googlebooks.model.GoogleBooksWrapper;
-import com.javaacademy.crawler.googlebooks.model.TotalItemsWrapper;
 import com.javaacademy.crawler.googlebooks.retrofit.RetrofitHelper;
 import org.testng.annotations.Test;
 import retrofit2.Call;
@@ -15,30 +13,31 @@ import static org.testng.Assert.assertNotNull;
 public class ControllerTest {
 
     public void getLimitedNumberBooksFromGoogleTest() {
-        Controller controller = new Controller();
         RetrofitHelper retrofitHelper = mock(RetrofitHelper.class);
         GoogleBookEndpoint endpoint = mock(GoogleBookEndpoint.class);
-        Callback<GoogleBooksWrapper> callback = mock(Callback.class);
+        Callback callback = mock(Callback.class);
         Call call = mock(Call.class);
-        when(endpoint.getFortyGoogleBooks(any())).thenReturn(call);
-        when(retrofitHelper.getGoogleBooksEndpoint()).thenReturn(endpoint);
-        controller.retrofitHelper = retrofitHelper;
-        controller.getLimitedNumberBooksFromGoogle(callback, 1, 1);
+        getControllerInstance(retrofitHelper, endpoint, call).getLimitedNumberBooksFromGoogle(callback, 1, 1);
         verify(endpoint, times(1)).getFortyGoogleBooks(any());
         verify(retrofitHelper, times(1)).getGoogleBooksEndpoint();
         verify(call, times(1)).enqueue(callback);
     }
 
-    public void getHowManyBooksThereAreTest() {
+    private Controller getControllerInstance(RetrofitHelper retrofitHelper,GoogleBookEndpoint endpoint, Call call) {
         Controller controller = new Controller();
-        RetrofitHelper retrofitHelper = mock(RetrofitHelper.class);
-        GoogleBookEndpoint endpoint = mock(GoogleBookEndpoint.class);
-        Callback<TotalItemsWrapper> callback = mock(Callback.class);
-        Call call = mock(Call.class);
+        when(endpoint.getFortyGoogleBooks(any())).thenReturn(call);
         when(endpoint.getNumberOfGoogleBooks(any())).thenReturn(call);
         when(retrofitHelper.getGoogleBooksEndpoint()).thenReturn(endpoint);
         controller.retrofitHelper = retrofitHelper;
-        controller.getHowManyBooksThereAre(callback);
+        return controller;
+    }
+
+    public void getHowManyBooksThereAreTest() {
+        RetrofitHelper retrofitHelper = mock(RetrofitHelper.class);
+        GoogleBookEndpoint endpoint = mock(GoogleBookEndpoint.class);
+        Callback callback = mock(Callback.class);
+        Call call = mock(Call.class);
+        getControllerInstance(retrofitHelper, endpoint, call).getHowManyBooksThereAre(callback);
         verify(endpoint, times(1)).getNumberOfGoogleBooks(any());
         verify(retrofitHelper, times(1)).getGoogleBooksEndpoint();
         verify(call, times(1)).enqueue(callback);
