1761a5eab5a86790ff0978cac1e80ed92a2c229f
Anna Gawda <a.m.gawda@gmail.com>
2017-09-10 17:37:43 +0200

    Added cors headers in main app class and two REST controllers for the main view and for retrieving one book by ISBN

---
+++ springapp/src/main/java/com/javaacademy/robot/controllers/BookRestAPIController.java
@@ -0,0 +1,38 @@
+package com.javaacademy.robot.controllers;
+
+import com.javaacademy.robot.model.BookDto;
+import com.javaacademy.robot.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author Anna Gawda
+ * 08.09.2017
+ */
+@RestController
+//@Controller
+public class BookRestAPIController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookRestAPIController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/api/books")
+    public BookDto bookRequestById(@RequestParam(value = "id") Long id) {
+        return bookService.getBookByIsbn(id);
+    }
+
+    @RequestMapping("/api/app")
+    public List<BookDto> bookList() {
+        return bookService.getAllBookDtos();
+    }
+}
