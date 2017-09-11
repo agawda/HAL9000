ab147bb02ac532b6e002fd7e223fac1c40d34aaf
Anna Gawda <a.m.gawda@gmail.com>
2017-09-11 10:01:28 +0200

    Added angular application -HTML layouts for the main page and for single book page -Services which retrieve data from REST API of the Spring Boot application -Sorting by title, author, price and promo

---
+++ rest-angular/src/app/book.ts
@@ -0,0 +1,16 @@
+export class Book {
+  industryIdentifier: number;
+  title: string;
+  subtitle: string;
+  authors: string[];
+  categories: string[];
+  smallThumbnail: string;
+  canonicalVolumeLink: string;
+  saleability: string;
+  listPriceAmount: number;
+  listPriceCurrencyCode: string;
+  retailPriceAmount: number;
+  retailPriceCurrencyCode: string;
+  shopName: string;
+  dateAdded: string;
+}
