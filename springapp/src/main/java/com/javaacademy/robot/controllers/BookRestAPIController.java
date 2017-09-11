e15c8f03e12c5f69c24ded4d819b2d555f7aba0e
Anna Gawda <a.m.gawda@gmail.com>
2017-09-11 09:06:18 +0200

    Removed unnecessary imports

--- springapp/src/main/java/com/javaacademy/robot/controllers/BookRestAPIController.java
+++ springapp/src/main/java/com/javaacademy/robot/controllers/BookRestAPIController.java
@@ -3,8 +3,6 @@ package com.javaacademy.robot.controllers;
 import com.javaacademy.robot.model.BookDto;
 import com.javaacademy.robot.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
