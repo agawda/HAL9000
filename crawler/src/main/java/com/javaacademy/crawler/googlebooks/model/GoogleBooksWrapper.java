31f8986f2959cfd1ed7d42b873d7aaa134841e65
Michal <michal_partacz@epam.com>
2017-09-12 12:49:29 +0200

    Added exception handling for google book converter;

--- crawler/src/main/java/com/javaacademy/crawler/googlebooks/model/GoogleBooksWrapper.java
+++ crawler/src/main/java/com/javaacademy/crawler/googlebooks/model/GoogleBooksWrapper.java
@@ -2,13 +2,15 @@ package com.javaacademy.crawler.googlebooks.model;
 
 import com.javaacademy.crawler.common.converters.GoogleBookConverter;
 import com.javaacademy.crawler.common.interfaces.BooksWrapper;
+import com.javaacademy.crawler.common.logger.AppLogger;
 import com.javaacademy.crawler.common.model.BookModel;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static com.javaacademy.crawler.common.logger.AppLogger.DEFAULT_LEVEL;
 
 /**
  * @author devas
@@ -21,7 +23,17 @@ public class GoogleBooksWrapper implements BooksWrapper {
 
     @Override
     public List<BookModel> getItems(GoogleBookConverter googleBookConverter) {
-        if(items == null) {return new ArrayList<>();}
-        return items.stream().map(googleBookConverter::convertToDto).collect(Collectors.toList());
+        if (items == null) {
+            return new ArrayList<>();
+        }
+        List<BookModel> models = new ArrayList<>();
+        for (BookItem bookItem : items) {
+            try {
+                models.add(googleBookConverter.convertToDto(bookItem));
+            } catch (NullPointerException e) {
+                AppLogger.logger.log(DEFAULT_LEVEL, "Exception while converting bookItem to BookModel");
+            }
+        }
+        return models;
     }
 }
